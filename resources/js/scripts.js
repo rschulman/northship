2917075b54903fb9e3f478a8aa17ea8cbe25eb9f
Ross Schulman <ross@rbs.io>
2017-01-16 22:52:19 -0500

    Client side timezone and geoloc gathering.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,17 @@
 var socket = new WebSocket("wss://rbs.io:8080");
 
 socket.onopen = function (event) {
-// Discover then send lat, long, and tz.
+    // Discover then send lat, long, and tz.
+    var d = new Date();
+    var tz = d.getTimeZoneOffset();
+    navigator.geolocation.getCurrentPosition().then(function(position){
+        var greeting = {
+            payload: "Hey Northship."
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            tz: tz
+        };
+    });
 
 }
 
