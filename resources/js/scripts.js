dbf7fd21044df9ca6bcc095381d1c90ad3ab3135
Ross Schulman <ross@rbs.io>
2017-01-18 15:17:34 -0500

    Bug fixes for scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,8 +3,8 @@ var socket = new WebSocket("wss://rbs.io:8080");
 socket.onopen = function (event) {
     // Discover then send lat, long, and tz.
     var d = new Date();
-    var tz = d.getTimeZoneOffset();
-    navigator.geolocation.getCurrentPosition().then(function(position){
+    var tz = d.getTimezoneOffset();
+    navigator.geolocation.getCurrentPosition(function(position){
         var greeting = {
             payload: "Hey Northship.",
             lat: position.coords.latitude,
